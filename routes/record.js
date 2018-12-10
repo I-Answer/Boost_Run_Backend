ef9167d8a5eccae30e24fe27731a8189b4e28aa5
huewilliams <4879sinwoo@naver.com>
2018-12-10 20:05:54 +0900

    CREATE: get/record/:nick API

--- routes/record.js
+++ routes/record.js
@@ -5,6 +5,29 @@ const User = require('../models').User;
 
 const router = express.Router();
 
+router.get('/:nick',async (req, res, next)=>{
+   try{
+       const user = await User.findOne({
+           where: {nick: req.params.nick}
+       });
+       if(user) {
+           res.json({
+               maxSpeed: user.maxSpeed,
+               maxTime: user.maxTime,
+           })
+       } else{
+           console.log('cannot found match user');
+           res.json({
+               status: 404,
+               message: 'cannot found match user'
+           })
+       }
+   } catch (err) {
+       console.error(err);
+       next(err);
+   }
+});
+
 router.post('/', async (req, res, next)=>{
     const user = await User.findOne({
         where: {nick: req.body.nick}
