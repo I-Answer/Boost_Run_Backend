f0e14cb356dad8a9f4da82ef29a3499f1e364e49
huewilliams <4879sinwoo@naver.com>
2018-12-10 19:38:52 +0900

    EDIT: post/record/ async await 방식 적용

--- routes/record.js
+++ routes/record.js
@@ -5,29 +5,29 @@ const User = require('../models').User;
 
 const router = express.Router();
 
-router.post('/', (req, res, next)=>{
-    User.findOne({
+router.post('/', async (req, res, next)=>{
+    const user = await User.findOne({
         where: {nick: req.body.nick}
-    })
-        .then((result)=>{
+    });
+        if(user) {
+            try{
+                const result = await Record.create({
+                    speed: req.body.speed,
+                    time: req.body.time,
+                    nick: req.body.nick,
+                });
+                res.json(result);
+            } catch (err) {
+                console.error(err);
+                next(err);
+            }
+        } else {
             console.log('cannot found match user');
             res.json({
                 status: 404,
                 message: 'cannot found match user'
             })
-        });
-    Record.create({
-        speed: req.body.speed,
-        time: req.body.time,
-        nick: req.body.nick,
-    })
-        .then((result)=>{
-            res.json(result);
-        })
-        .catch((err)=>{
-            console.error(err);
-            next(err);
-        })
+        }
 });
 
 module.exports = router;
