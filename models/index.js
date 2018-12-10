793f93aff7542bd6d8afab960f40de45e2dd3243
huewilliams <4879sinwoo@naver.com>
2018-12-10 14:48:03 +0900

    CREATE: sequelize config

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+
+const db = {};
+
+const sequelize = new Sequelize(
+    'boostrun', // DB 이름
+    'root', // 유저명
+    'espada8012!', // 패스워드
+    {
+        'host':'127.0.0.1', //사용할 호스트
+        'dialect':'mysql' //사용할 DB 종류
+    }
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require('./user')(sequelize, Sequelize);
+db.Record = require('./Record')(sequelize, Sequelize);
+
+db.User.hasMany(db.Record, { foreignKey: 'nick', sourceKey: 'nick' });
+db.Record.belongsTo(db.User, { foreignKey: 'nick', sourceKey: 'nick'});
+
+module.exports = db;
