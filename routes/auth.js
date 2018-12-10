3a6e0eb7bacd6437933a8c37a965435b177e6752
huewilliams <4879sinwoo@naver.com>
2018-12-10 19:28:08 +0900

    EDIT: post/auth/user API async await 방식으로 바꿈

--- routes/auth.js
+++ routes/auth.js
@@ -5,25 +5,28 @@ const User = require('../models').User;
 const router = express.Router();
 
 // 루트 라우터
-router.post('/user', (req, res, next)=>{
-    User.findOne({
+router.post('/user', async (req, res, next)=>{
+    const user = await User.findOne({
         where: {nick: req.body.nick}
     })
-        .then((result) => {
-        console.log('duplicate nickname error');
-        res.json({
-            status: 409,
-            message: 'duplicate nickname error'
-        })});
-    User.create({
-        id: req.body.id,
-        password: req.body.password,
-        nick: req.body.nick,
-    })
-        .then((result)=>{
-            console.log(result);
-            res.json(result).status(201);
-        })
+        if(user){
+        console.log(user);
+         res.json({
+                status: 409,
+                message: 'duplicate nickname error'
+            });
+        }
+        else{
+            await User.create({
+                id: req.body.id,
+                password: req.body.password,
+                nick: req.body.nick,
+            })
+                .then((result)=>{
+                    console.log(result);
+                    res.json(result).status(201);
+                })
+        }
 });
 
 module.exports = router;
